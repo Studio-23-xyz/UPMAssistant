9ed7e7f7976bd016406a2d787043012337251232
gitOpu <73388427+gitOpu@users.noreply.github.com>
2023-10-05 19:16:11 +0600

    backup : few model  updating on progress

---
+++ Assets/Packages/com.studio23.ss2.upmassistant/Editor/Models/PackageJsonData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Studio23.SS2.UPMAssistant.Editor
+{
+    public class PackageJsonData
+    {
+        public string name;
+        public string version;
+        public string displayName;
+        public string description;
+        public string unityVersion;
+        public string unityRelease;
+        public string documentationUrl;
+        public string changelogUrl;
+        public string licensesUrl;
+        public string scopedRegistryName;
+        public string scopedRegistryUrl;
+        public List<string> scopedRegistryScopes = new List<string>();
+        public List<PackageDependency> dependencies = new List<PackageDependency>();
+        public List<string> keywords = new List<string>();
+        public string authorName;
+        public string authorEmail;
+        public string authorUrl;
+    }
+}
