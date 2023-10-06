cf0f7b3ab474b7d223581626749eb154bc2a6e2f
gitOpu <73388427+gitOpu@users.noreply.github.com>
2023-10-06 17:56:06 +0600

    backup : json updated

--- Editor/Models/PackageJsonData.cs
+++ Editor/Models/PackageJsonData.cs
@@ -15,7 +15,7 @@ namespace Studio23.SS2.UPMAssistant.Editor
         public string licensesUrl;
         public string scopedRegistryName;
         public string scopedRegistryUrl;
-        public List<string> scopedRegistryScopes = new List<string>();
+        public List<ScopedRegistry> scopedRegistryScopes = new List<ScopedRegistry>();
         public List<PackageDependency> dependencies = new List<PackageDependency>();
         public List<string> keywords = new List<string>();
         public string authorName;
